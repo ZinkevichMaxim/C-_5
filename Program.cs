f356f1c0f486addd7a3084218c80f516bb825749
ZINKEVICH Maxim <drmvz@mail.ru>
2023-06-04 18:12:55 +0300

    fin

--- Program.cs
+++ Program.cs
@@ -97,3 +97,75 @@ Console.WriteLine();
 Console.WriteLine($"Сумма положительных чисел равна {ress[0]} Cумма отрицательных чисел равна {ress[1]}");
 
 
+// Задача 32: Напишите программу замена элементов массива: положительные элементы замените на соответствующие отрицательные, и наоборот.
+// [-4, -8, 8, 2] -> [4, 8, -8, -2]
+int[] Massiv(int size, int minValue, int maxValue)
+{
+int[] res = new int[size]; // создаем массив
+for (int i = 0; i < size; i++)
+{
+res[i] = new Random().Next(minValue, maxValue + 1);
+Console.Write($"{res[i]} ");
+
+}
+return res;
+}
+// void ChangeNum(int[] array)
+// {
+
+// foreach (int el in array)
+// {
+// Console.Write($"{el*(-1)} ");
+
+// }
+
+// }
+void ChangeNum2(int[] array)
+{
+int i = 0;
+foreach (int el in array)
+{
+
+array[i]=el*(-1);
+Console.Write($"{array[i]} ");
+i++;
+
+}
+
+}
+void ShowResult(int [] num){
+Console.WriteLine($"Сумма положительных чисел = {num[0]}, сумма отрицательных чисел = {num[1]}");
+}
+
+int[] xyz = Massiv(12, -9, 9);
+Console.WriteLine(" ");
+ChangeNum2(xyz);
+
+// Задача 33: Задайте массив. Напишите программу, которая определяет, присутствует ли заданное число в массиве.
+// 4; массив [6, 7, 19, 345, 3] -> нет
+// -3; массив [6, 7, 19, 345, 3] -> да
+int[] massiv = { 6, 7, 19, 345, 3 };
+void FindNum(int[] array, int x)
+{
+int i = 0;
+int length = array.Length;
+bool flag = false;
+while (i < length)
+{
+
+if (x == array[i])
+{
+flag = true;
+}
+i++;
+}
+if (flag) {
+Console.WriteLine($"Цифра {x} имеется в массиве");
+
+}
+else {
+Console.WriteLine($"Цифра {x} нет в массиве");
+}
+}
+Console.WriteLine(string.Join(", ", massiv));
+FindNum(massiv, 345);
